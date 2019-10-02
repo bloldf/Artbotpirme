b9d655b2a898d6fcc2c0d7661d107c34a53fc1e7
bloldf <48572660+bloldf@users.noreply.github.com>
2019-10-02 20:12:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -922,6 +922,7 @@ client.on('message',async Epic => {
   var prefix = "-" ;
   if(Epic.content.startsWith(prefix + "vonline")) {
   if(!Epic.guild.member(Epic.author).hasPermissions('MANAGE_CHANNELS')) return Epic.reply(':x: **I Dont Have Permissions**');
+    message.channel.send('✅| **تم عمل الروم بنجاح**');
   if(!Epic.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return Epic.reply(':x: **You Dont Have Permissions**');
   Epic.guild.createChannel(`Voice Online : [ ${Epic.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
     console.log(`Voice Online Is Activation In ${Epic.guild.name}`);
